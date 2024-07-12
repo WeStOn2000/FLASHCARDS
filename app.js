044ae8950a76816516c6174f2878461e248596e3
WeStOn2000 <161762685+WeStOn2000@users.noreply.github.com>
2024-07-11 21:17:41 -0400

    ..

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
-
+const bodyParser =  require('body-parser');
+const cookieParser = require('cookie-parser')
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false}));
 app.set('view engine', 'pug');
+app.use(cookieParser());
 
-app.get('/', (req,res) => {
- res.render('index');
+ const  mainRoutes = require('./routes');
+ const cardRoutes = require('./routes/cards');
+
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
+
+app.use((req,res,next) => {
+ const err = new Error ('Not Found');
+ err.status = 404;
+ next(err);
 });
 
-app.get('/cards', (req,res) => {
-    res.render('card',{prompt : "Who is Buried in Grant's tomb"});
-   });
+app.use((err,req,res,next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render("error");
+});
 
 app.listen(3000, () => {
-console.log('Port Activated:3000')
+console.log('Port Activated:3000');
 });
